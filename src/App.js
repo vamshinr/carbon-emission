9c24a5ad4744e7ee3da4b1d1ae754f9188182d24
Likhith Muthavarapu <likhithmuthavarapu@Likhiths-MacBook-Pro.local>
2022-10-02 15:04:33 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import Input from './components/Input';
 import Login from './components/Login';
+import NavScrollExample from './components/navbar'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div>
+      <NavScrollExample />
     <div className="App"    >
+
       <div>
         Enter HPT number:
       </div>
