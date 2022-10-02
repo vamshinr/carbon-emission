ab9e1cb922b9f093430501d2e2ca9f4be9603c73
kkeerthana23 <kkeerthana1997@gmail.com>
2022-10-02 14:48:33 -0700

    Icon Chnages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Input from './components/Input';
 import Login from './components/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
