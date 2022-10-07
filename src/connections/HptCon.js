61e7f53b5a24e6079c92bf3e4bae4300e598d3c2
vamshireddy <vnagireddy@csus.edu>
2022-10-06 17:38:10 -0700

    changes to vendia

--- src/connections/HptCon.js
+++ src/connections/HptCon.js
@@ -5,9 +5,93 @@ const clientauth = clientdetails();
 class HptCon{
     async hpt_fetch() {
         const hptResponse = await clientauth.hornetPowerTools.list();
-        console.log(hptResponse.items);
+        console.log("hpt data: ", hptResponse.items);
         return (hptResponse);
     }
+
+    async hpt_fetch_by_number(hpt, objtype){
+
+        const hptdata =  await clientauth.hornetPowerTools.list(
+            {
+             filter: {
+                 SerialNumber: {
+                   contains: hpt,
+                 },
+            }
+        }
+        );
+
+        if (objtype === 'motor')
+        {
+            const motorData = await clientauth.motor.list(
+                {
+                 filter: {
+                     serialNumber: {
+                       contains: hptdata.items[0].motorId,
+                     },
+                }
+            }
+            );
+
+            return motorData.items;
+        }
+
+        if (objtype === 'battery'){
+
+            const batteryData = await clientauth.battery.list(
+                {
+                 filter: {
+                     serialNumber: {
+                       contains: hptdata.items[0].BatteryId,
+                     },
+                }
+            }
+            );
+
+            return batteryData.items;
+
+        }
+        if (objtype === 'seatransport'){
+
+            const seadata = await clientauth.seaTransportation.list(
+                {
+                 filter: {
+                     serialNumber: {
+                       contains: hptdata.items[0].seaTransportId,
+                     },
+                }
+            }
+            );
+
+            return seadata.items;
+
+        }
+        if (objtype === 'groundtransport'){
+
+            const grounddata = await clientauth.groundTransportation.list(
+                {
+                 filter: {
+                     serialNumber: {
+                       contains: hptdata.items[0].groundTransportId,
+                     },
+                }
+            }
+            );
+
+            return grounddata.items;
+
+        }
+
+         console.log("HPT Items :",hptdata.items)
+         console.log("HPT ID:", hptdata.items[0].SerialNumber);
+         console.log("HPT Moter :", hptdata.items[0].motorId);
+         console.log("HPT Battery :", hptdata.items[0].BatteryId);
+         console.log("HPT sea :", hptdata.items[0].seaTransportId);
+         console.log("HPT ground :", hptdata.items[0].groundTransportId);
+         return hptdata.items;
+     }
+
+
 }
 
 export default new HptCon();
--- src/pages/ToolView.js
+++ src/pages/ToolView.js
@@ -3,11 +3,7 @@ import React from "react";
 import {FaTools} from 'react-icons/fa';
 import drill from './logos/drill.png';
 import Typography from '@mui/material/Typography';
-import BatteryCon from '../connections/BatteryCon';
-import MotorCon from "../connections/MotorCon";
 import HptCon from "../connections/HptCon";
-import groundTransportCon from "../connections/groundTransportCon";
-import SeaTransportCon from "../connections/SeaTransportCon";
 import { useState } from "react";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -43,97 +39,39 @@ const Item = styled(Paper)(({ theme }) => ({
 
 
 export default function ToolView(params){
-    console.log(params.hpt);
-    const [hptId, setHPTId] = useState();
-    const [hptco2,setHPTco2] = useState(0);
-    const [seaTransportationco2,setseaTransportationco2] = useState(0);
-    const [groundTransportationco2,setgroundTransportationco2] = useState(0);
-    const [userAuthco2,setuserAuthco2] = useState(0);
-    const [motorSupplierco2,setmotorSupplierco2] = useState(0);
-    const [batterySupplierco2, setbatterySupplierco2] = useState(0);
-    const [batteryid,setbatteryid] = useState();
-    const [motorid,setmotorid] = useState();
-    const [seaid,setseaid] = useState();
-    const [groundid,setgroundid] = useState();
 
-    HptCon.hpt_fetch().then(function(result) {
-        for (var i = 0; i < result.items.length; i++) {
-          if (result.items[i].SerialNumber === params.hpt){
-            setHPTId(result.items[i].SerialNumber);
-            console.log("hpt found")
-            console.log("hpt:", result.items[i]);
-            setHPTco2(result.items[i].co2);
-            setbatteryid(result.items[i].BatteryId);
-            setmotorid(result.items[i].motorId);
-            setseaid(result.items[i].seaTransportId);
-            setgroundid(result.items[i].groundTransportId);
-          }
-          else{
-            setHPTco2(0);
-            setbatterySupplierco2(0);
-            setmotorSupplierco2(0);
-            setseaTransportationco2(0);
-            setgroundTransportationco2(0);
-            rows.push(createData('Battery Supplier CO'+<sub>2</sub>, 0));
-            rows.push(createData('Motor Supplier CO'+<sub>2</sub>, 0));
-            rows.push(createData('Sea Transportation CO'+<sub>2</sub>, 0));
-            rows.push(createData('Ground Transpor CO'+<sub>2</sub>, 0));
-          }
-        }
-      });
+    // const [hptId, setHPTId] = useState('');
+    const [hptco2,setHPTco2] = useState([]);
+    const [seaTransportationco2,setseaTransportationco2] = useState([]);
+    const [groundTransportationco2,setgroundTransportationco2] = useState([]);
+    // const [userAuthco2,setuserAuthco2] = useState([]);
+    const [motorSupplierco2,setmotorSupplierco2] = useState([]);
+    const [batterySupplierco2, setbatterySupplierco2] = useState([]);
+  
+    async function co2data(){
+        const hptTotal = await HptCon.hpt_fetch_by_number(params.hpt)
+        setHPTco2([...hptTotal])
+  
+        const motor = await HptCon.hpt_fetch_by_number(params.hpt, 'motor')
+        setmotorSupplierco2([...motor])
+  
+        const battery = await HptCon.hpt_fetch_by_number(params.hpt, 'battery')
+        setbatterySupplierco2([...battery])
 
-      if (hptco2 !==0){
-    BatteryCon.battery_fetch().then(function(result) {
-        for (var i = 0; i < result.items.length; i++) {
-          if (result.items[i].serialNumber === batteryid){
-            console.log("battery found "+result.items[i].co2);
-            setbatterySupplierco2(result.items[i].co2);
-            rows.push(createData('Battery Supplier CO'+<sub>2</sub>, result.items[i].co2));
-          }
-        }
-      });
+        const seat =  await HptCon.hpt_fetch_by_number(params.hpt, 'seatransport')
+        setseaTransportationco2([...seat])
 
-
-    MotorCon.motor_fetch().then(function(result) {
-    for (var i = 0; i < result.items.length; i++) {
-        if (result.items[i].serialNumber === motorid){
-        setmotorSupplierco2(result.items[i].co2);
-        rows.push(createData('Motor Supplier CO'+<sub>2</sub>, result.items[i].co2));
-        }
-        else{
-        setmotorSupplierco2(0);
-        }
+        const groundt = await HptCon.hpt_fetch_by_number(params.hpt, 'groundtransport')
+        setgroundTransportationco2([...groundt])
     }
-    });
+    co2data();
+    const totalBatteryCo2 = batterySupplierco2.reduce(function (res, item) {return res + item.co2;}, 0);
+    const totalMotorCo2 = motorSupplierco2.reduce(function (res, item) {return res + item.co2;}, 0);
+    const totalseaco2 = seaTransportationco2.reduce(function (res, item) {return res + item.co2;}, 0);
+    const totalgroundco2 = groundTransportationco2.reduce(function (res, item) {return res + item.co2;}, 0);
+    const totalHPTCo2 = hptco2.reduce(function (res, item) {return res + item.co2;}, 0);
 
-
-    SeaTransportCon.sea_fetch().then(function(result) {
-    for (var i = 0; i < result.items.length; i++) {
-        if (result.items[i].trackingNumber === seaid){
-        setseaTransportationco2(result.items[i].co2);
-        rows.push(createData('Sea Transportation CO'+<sub>2</sub>, result.items[i].co2));
-        }
-        else{
-        setseaTransportationco2(0);
-        }
-    }
-    });
-
-
-    groundTransportCon.ground_fetch().then(function(result) {
-    for (var i = 0; i < result.items.length; i++) {
-        if (result.items[i].trackingNumber === groundid){
-        setgroundTransportationco2(result.items[i].co2);
-        rows.push(createData('Ground Transpor CO'+<sub>2</sub>, result.items[i].co2));
-        }
-        else{
-        setgroundTransportationco2(0);
-        }
-    }
-    });
-
-}
-    //HTML rendering
+    console.log("data:  ",totalseaco2,totalgroundco2);
     return (
         <>
         <div className="">
@@ -150,68 +88,44 @@ export default function ToolView(params){
                     {/* <img src={drill} alt='Sacramento State' style={{width: '225px', height: '200px'}} /> */}
                 </div>
                 <div className="align-self-center col-lg-6 col-md-6 col-sm-6 col-xs-12" style={{fontSize:'25px', fontWeight:'bold'}}>
-                    <span style={{color:'#00573d'}}> Total CO<sub>2</sub> Emission : </span>   {hptco2}
+                    <span style={{color:'#00573d'}}> Total CO<sub>2</sub> Emission : </span>   {totalHPTCo2}
                 </div>
             </div>
             <div className="row" style={{paddingTop:'18px'}}>
-                <div>
-                    <Accordion>
-                    <AccordionSummary expandIcon={<ExpandMoreIcon style={{color: '#fff'}}/>} aria-controls="panel1a-content" id="panel1a-header" style={{backgroundColor:'#00573d', color:'#fff'}}>
-                        <Typography>CO<sub>2</sub> Emission Breakdown</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        {/* <Typography> */}
-                        <Box sx={{ width: '100%' }}>
-                            <Stack spacing={2}>
-                                <Item>
-                                    <span style={{color:'#a17f0f'}}>
-                                        Battery Supplier CO<sub>2</sub> Emission : <span style={{color:'#00573d', fontWeight:'bold'}}>{batterySupplierco2} </span>
-                                    </span>
-                                </Item>
-                                <Item>
-                                    <span style={{color:'#a17f0f'}}>
-                                        Motor Supplier CO<sub>2</sub> Emission : <span style={{color:'#00573d', fontWeight:'bold'}}>{motorSupplierco2}</span>
-                                    </span>
-                                </Item>
-                                <Item>
-                                    <span style={{color:'#a17f0f'}}>
-                                        Ground Transportation CO<sub>2</sub> Emission : <span style={{color:'#00573d', fontWeight:'bold'}}>{groundTransportationco2}</span>
-                                    </span>
-                                </Item>
-                                <Item>
-                                    <span style={{color:'#a17f0f'}}>
-                                        Sea Transportation CO<sub>2</sub> Emission : <span style={{color:'#00573d', fontWeight:'bold'}}>{seaTransportationco2}</span>
-                                    </span>
-                                </Item>
-                            </Stack>
-                            {/* <TableContainer component={Paper}>
-                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                                    <TableHead>
-                                    <TableRow>
-                                        <TableCell></TableCell>
-                                        <TableCell align="center">Component</TableCell>
-                                        <TableCell align="center">CO<sub>2</sub>(g)</TableCell>                                
-                                    </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                    {rows.map((row) => (
-                                        <TableRow
-                                        key={row.Component}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                        <TableCell component="th" scope="row">
-                                            {row.Component}
-                                        </TableCell>
-                                        <TableCell align="right">{row.Emission}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer> */}
-                        </Box>
-                        {/* </Typography> */}
-                    </AccordionDetails>
-                    </Accordion>
+            <div>
+                <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon style={{color: '#fff'}}/>} aria-controls="panel1a-content" id="panel1a-header" style={{backgroundColor:'#00573d', color:'#fff'}}>
+                    <Typography>CO<sub>2</sub> Emission Breakdown</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {/* <Typography> */}
+                    <Box sx={{ width: '100%' }}>
+                        <Stack spacing={2}>
+                            <Item>
+                                <span style={{color:'#00573d'}}>
+                                    Battery Supplier CO<sub>2</sub> Emission : {totalBatteryCo2}
+                                </span>
+                            </Item>
+                            <Item>
+                                <span style={{color:'#00573d'}}>
+                                    Motor Supplier CO<sub>2</sub> Emission : {totalMotorCo2}
+                                </span>
+                            </Item>
+                            <Item>
+                                <span style={{color:'#00573d'}}>
+                                    Ground Transportation CO<sub>2</sub> Emission : {totalseaco2}
+                                </span>
+                            </Item>
+                            <Item>
+                                <span style={{color:'#00573d'}}>
+                                    Sea Transportation CO<sub>2</sub> Emission : {totalgroundco2}
+                                </span>
+                            </Item>
+                        </Stack>
+                    </Box>
+                    {/* </Typography> */}
+                </AccordionDetails>
+                </Accordion>
                 </div>
             </div>
         </div>
