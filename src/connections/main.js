6734257ec380bdc7769a46ebc1257723101b7852
kkeerthana23 <kkeerthana1997@gmail.com>
2022-11-29 21:09:46 -0800

    total co2 in hpt add-edit

--- src/connections/main.js
+++ src/connections/main.js
@@ -5,7 +5,7 @@ function clientdetails() {
     const client = createVendiaClient({
     apiUrl: 'https://uytqy9yq0l.execute-api.us-west-2.amazonaws.com/graphql/',
     websocketUrl: 'wss://e37irfs036.execute-api.us-west-2.amazonaws.com/graphql',
-    apiKey: 'AdT9KwYvNLEwyXUqXKs9PLVSm9Lok5RAyKsLEe3YLuY4'
+    apiKey: 'DEnnS31xC4V6ajfEcqzTBZRqjuAFTMAfFiFJuhYy4pAJ'
     });
 
     const {entities} = client;
